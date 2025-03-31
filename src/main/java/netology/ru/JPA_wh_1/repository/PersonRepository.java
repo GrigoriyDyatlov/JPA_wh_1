4087d48e8acbf4930a38431136ac7ddd453baacd
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2025-03-31 20:39:47 +0300

    refactor code

--- src/main/java/netology/ru/JPA_wh_1/repository/PersonRepository.java
+++ src/main/java/netology/ru/JPA_wh_1/repository/PersonRepository.java
@@ -2,19 +2,15 @@ package netology.ru.JPA_wh_1.repository;
 
 import netology.ru.JPA_wh_1.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    @Query("select p from Person p where p.cityOfLiving = ?1")
     List<Person> findAllByCityOfLiving(String city);
 
-    @Query("select p from Person p where p.age < ?1 order by p.age")
     List<Person> findByAgeLessThanOrderByAgeAsc(Integer age);
 
-    @Query("select p from Person p where upper(p.name) = upper(?1) and upper(p.surname) = upper(?2)")
     Optional<List<Person>> findByNameIgnoreCaseAndSurnameIgnoreCase(String name, String surname);
 
 
